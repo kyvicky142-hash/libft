4fd8109f978f7fd7c5778728008d9af608e417d2
vshanmug <vshanmug@student.42kl.edu.my>
2026-04-18 14:59:33 +0800

    test5

--- libft.h
+++ libft.h
@@ -18,17 +18,19 @@
 # include <unistd.h>
 # include <string.h>
 
-int	ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_isascii(int c);
+int		ft_atoi(const char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_isascii(int c);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 char	*ft_itoa(int n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strchr(const char *s, int c);
